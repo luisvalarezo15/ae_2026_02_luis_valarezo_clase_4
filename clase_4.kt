bcd33c4ca40eec6d43eaa2ce2d8a7f7d32df5693
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 09:04:38 -0500

    clase_04 2

--- clase_4.kt
+++ clase_4.kt
@@ -1,4 +1,20 @@
-fun main (){
+//fun main (){
     val name = "jorge"
     println ("Hola $name")
+//}
+fun main() {
+    val student = Student (
+    id = 1,
+    name = "Luis",
+    emanil = "luisval@gmail.com"    
+        
+    )
+    println(student)
 }
+data class Student (
+    val id : Long,
+    val name : String,
+    val emal : String,
+    val grade : int,
+    val isActive : Boolean
+)
