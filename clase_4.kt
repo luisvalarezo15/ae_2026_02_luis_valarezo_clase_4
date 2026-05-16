6b1aa87d5adbfc2a12ba0f0e3ed99cba1a7ceeec
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 09:42:36 -0500

    clase_04 8

--- clase_4.kt
+++ clase_4.kt
@@ -159,7 +159,7 @@
 //}
 ///7
 //ejercicio 7
-fun main() {
+//fun main() {
 
     val student1 = Student(
         id = 1,
@@ -193,8 +193,16 @@ fun main() {
         if (miVariable.isActive) {
             println("${miVariable.name} - ${miVariable.grade}")
         }
-}
+//}
 
+//data class Student(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val grade: Int,
+    val isActive: Boolean
+//)
+//ejercicio 8
 data class Student(
     val id: Long,
     val name: String,
@@ -203,6 +211,42 @@ data class Student(
     val isActive: Boolean
 )
 
+fun main() {
+
+    val student1 = Student(
+        id = 1,
+        name = "Luis",
+        email = "luisval@gmail.com",
+        grade = 8,
+        isActive = true
+    )
+
+    val student2 = Student(
+        id = 2,
+        name = "Luisa",
+        email = "luisaval@gmail.com",
+        grade = 9,
+        isActive = true
+    )
+
+    val student3 = Student(
+        id = 3,
+        name = "Carlos",
+        email = "Carlosval@gmail.com",
+        grade = 4,
+        isActive = false
+    )
+
+    val students = listOf(student1, student2, student3)
+
+    println(students)
+
+    for (miVariable in students) {
+        if (miVariable.isActive) {
+            println("${miVariable.name} - ${miVariable.grade}")
+        }
+    }
+}
 
 
 
