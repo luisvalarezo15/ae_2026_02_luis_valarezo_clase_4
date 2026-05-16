3b63ee87736bfb0b746b065cee5e8ba08f046f3d
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 09:13:06 -0500

    clase_04 4

--- clase_4.kt
+++ clase_4.kt
@@ -21,6 +21,28 @@
     //val isActive : Boolean
 //)
 ///ejercicio 3
+//fun main() {
+    //val student = Student (
+    //id = 1,
+    //name = "Luis",
+    //emanil = "luisval@gmail.com"    
+    //grade = 8,
+    //isActive = true    
+    //)
+    //println(student)
+    //if (Student.isActive){
+        println("El estudiante ${student.name} esta activo")
+    //}
+    else{
+        println("El estudiante ${student.name} no esta activo")
+    }
+//}
+//data class Student (
+    val id : Long,
+    val name : String,
+    val emal : String
+//)
+///ejrcicio 4
 fun main() {
     val student = Student (
     id = 1,
@@ -36,9 +58,16 @@ fun main() {
     else{
         println("El estudiante ${student.name} no esta activo")
     }
+    val status : String = if (student.isActive){
+        "Activo"
+    }
+    else{
+        "Inactivo"
+    }
 }
 data class Student (
     val id : Long,
     val name : String,
-    val emal : String
-)
+    val emal : String,
+    val grade : int
+    val isActive : Boolean
