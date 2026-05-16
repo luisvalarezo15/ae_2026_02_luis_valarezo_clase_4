a72ac29ffa1f6db0cd3ac5d4bca6ac8b827b2c99
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 09:45:13 -0500

    clase_04 10

--- clase_4.kt
+++ clase_4.kt
@@ -249,15 +249,15 @@
 //}
 
 //ejercicio 9
-data class Student(
+//data class Student(
     val id: Long,
     val name: String,
     val email: String,
     val grade: Int,
     val isActive: Boolean
-)
+//)
 
-fun main() {
+//fun main() {
 
     val student1 = Student(
         id = 1,
@@ -301,8 +301,61 @@ fun main() {
     }
 
     println("${student1.name}: $result")
+//}
+//ejercicio 10
+data class Student(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val grade: Int,
+    val isActive: Boolean
+)
+
+fun getStudentResult(student: Student): String {
+    return when (student.grade) {
+        in 9..10 -> "Sobresaliente"
+        in 7..8 -> "Aprobado"
+        in 5..6 -> "Reprobado"
+        else -> "Nota invalida"
+    }
 }
 
+fun main() {
+
+    val student1 = Student(
+        id = 1,
+        name = "Luis",
+        email = "luisval@gmail.com",
+        grade = 8,
+        isActive = true
+    )
+
+    val student2 = Student(
+        id = 2,
+        name = "Luisa",
+        email = "luisaval@gmail.com",
+        grade = 9,
+        isActive = true
+    )
+
+    val student3 = Student(
+        id = 3,
+        name = "Carlos",
+        email = "Carlosval@gmail.com",
+        grade = 4,
+        isActive = false
+    )
+
+    val students = listOf(student1, student2, student3)
+
+    println(students)
+
+    for (miVariable in students) {
+        if (miVariable.isActive) {
+            println("${miVariable.name} - ${miVariable.grade} - ${getStudentResult(miVariable)}")
+        }
+    }
+}
 
 
 
